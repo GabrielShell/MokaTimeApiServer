bf989549c9bd6812982d1f64a5e0c6ebc00dae58
Gabriel Shell <2794276764@qq.com>
2018-07-31 15:22:20 +0800

    建立还款计划控制器

---
+++ application/mkapi/controller/RepayPlan.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\mkapi\controller;
+
+use think\Controller;
+use app\mkapi\model\Users;
+
+//TODO 正式环境将继承类改成Common
+class RepayPlan extends Controller{
+    public function getPlan(Request $request){
+        $userSeries = $request->post('series');
+        $userId = Users::where(['series'=>$user_series])->value('id');
+
+    }
+}
+//FIXME dfa
