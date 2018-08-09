5d17573e2518a70cb3a304f19f258a0ef69bed69
linqiaoling <813771246@qq.com>
2018-08-09 19:33:08 +0800

    更新登录

--- application/mkapi/controller/User.php
+++ application/mkapi/controller/User.php
@@ -159,8 +159,8 @@ class User extends Common{
 		$data['series'] = getUserId(6,$data['phone']);
 		$User->data($data);
 		if($User->save()){
-			echo $data['series'];
-			exit();
+			// echo $data['series'];
+			// exit();
 			return $data['series'];
 		}
 	}
