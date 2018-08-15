20f07b4333d236c3f22f9960a01c60bfa39d3b10
linqiaoling <813771246@qq.com>
2018-08-15 20:10:41 +0800

    测试美洽消息推送

---
+++ application/mkapi/controller/Push.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\mkapi\controller;
+use think\Controller;
+use think\Request;
+use think\Db;
+use think\Session;
+use think\Log;
+class Push extends Controller{
+	public function pushMeiqia(){
+		$data = $_REQUEST;
+        if(empty($data)){
+            $data = file_get_contents("php://input");
+        }
+
+        write_to_log('【美洽消息推送】'.$data,'mkapi/log/');
+	}
+}
