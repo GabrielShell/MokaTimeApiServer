26bc3c3962a568fba69d0b24f629c6d3daa71705
linqiaoling <813771246@qq.com>
2018-08-16 14:59:44 +0800

    美洽测试：数据流

--- application/mkapi/controller/Push.php
+++ application/mkapi/controller/Push.php
@@ -9,17 +9,14 @@ use app\mkapi\common\Merqia\DTSigner;
 class Push extends Controller{
 	public function pushMeiqia(){
 		$secret_key = "$2a$12$M3Je3l0qTy4Gy12E6PqviuOI25cfRRpEHFI.ARteS/v8TVhBeX3na";
-		
-		
-		write_to_log("【file_get_contents('php://input')】".file_get_contents("php://input"),'mkapi/log/');
-		write_to_log("【$_REQUEST】".$_REQUEST,'mkapi/log/');
-		write_to_log("【http_get_request_body()】".http_get_request_body(),'mkapi/log/');
 		$data = $_REQUEST;
         if(empty($data)){
             $data = file_get_contents("php://input");
         }
+
+        write_to_log("【美洽美洽】".$data,'mkapi/log/');
         $meiqia_sign = $_SERVER['HTTP_AUTHORIZATION'];
         $data = json_decode($data);
-        $series = $data['customizedId'];
+        //$series = $data['customizedId'];
 	}
 }
