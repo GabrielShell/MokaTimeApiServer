70c890189e4b035f820147ce5bcb07c8e98a698e
linqiaoling <813771246@qq.com>
2018-08-16 16:38:19 +0800

    测试离线推送

--- application/mkapi/controller/Push.php
+++ application/mkapi/controller/Push.php
@@ -9,21 +9,21 @@ use app\mkapi\common\UMeng\Umeng;
 class Push extends Controller{
 	public function pushMeiqia(){
 		$secret_key = '$2a$12$M3Je3l0qTy4Gy12E6PqviuOI25cfRRpEHFI.ARteS/v8TVhBeX3na';
-		$data = $_REQUEST;
-        if(empty($data)){
-            $data = file_get_contents("php://input");
-            write_to_log("【返回数据流】".$data,"mkapi/log/");
-        }
-      
+		// $data = $_REQUEST;
+  //       if(empty($data)){
+  //           $data = file_get_contents("php://input");
+  //           write_to_log("【返回数据流】".$data,"mkapi/log/");
+  //       }
+      	$data = file_get_contents("php://input");
         //美洽验签
-        //$signer = new DTSigner($secret_key);
-        // if($signer->sign(json_decode($data)) !== $_SERVER['HTTP_AUTHORIZATION']){
-        // 	write_to_log("【美洽验签成功】");
-        // 	exit();
-        // }else{
-        // 	write_to_log("【美洽验签失败】sign1=".$signer->sign($data)." sign2=".$_SERVER['HTTP_AUTHORIZATION']);
-        // }
-         write_to_log("【美洽推送数据】".$data,"mkapi/log/");
+        $signer = new DTSigner($secret_key);
+        if($signer->sign(json_decode($data)) !== $_SERVER['HTTP_AUTHORIZATION']){
+        	write_to_log("【美洽验签成功】","mkapi/log/");
+        	exit();
+        }else{
+        	write_to_log("【美洽验签失败】sign1=".$signer->sign($data)." sign2=".$_SERVER['HTTP_AUTHORIZATION'],"mkapi/log/");
+        }
+        write_to_log("【美洽推送数据】".$data,"mkapi/log/");
         $data = json_decode($data);
         $series = $data['customizedId'];
         // 判断token是否存在
