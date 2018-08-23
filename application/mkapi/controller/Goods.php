3d1d462eecdc25dc1e6426f0c3bdd5240f58e4ec
linqiaoling <813771246@qq.com>
2018-08-23 16:39:24 +0800

    修改获取商品列表

--- application/mkapi/controller/Goods.php
+++ application/mkapi/controller/Goods.php
@@ -11,14 +11,20 @@ use think\Log;
 class Goods extends Common{
 	//获取商品列表
 	public function goodsList(){
-		$goodsList = Db::name('goods')->field('id as goods_id,goods_name,goods_img,goods_money,is_limit,limit_money')->select();
-		foreach ($goodsList as $key => $value) {
+		$goodsList = Db::name('goods')->field('id as goods_id,goods_name,goods_money,is_limit,limit_money')->select();
+		my_json_encode(10000,'success',$goodsList);
+	}
+
+	//获取商品图片列表
+	public function goodsImgList(){
+		$goodsImgList = Db::name('goods')->field('id as goods_id,goods_img')->select();
+		foreach ($goodsImgList as $key => $value) {
 			$handle = fopen($value['goods_img'],'r');
 			$imgData = fread($handle,filesize($value['goods_img']));
-			$goodsList[$key]['goods_img'] = $imgData;
+			$goodsImgList[$key]['goods_img'] = base64_encode($imgData);
 			fclose($handle);
 		}
-		my_json_encode(10000,'success',$goodsList);
+		my_json_encode(10000,'success',$goodsImgList);
 	}
 
 	//获取商品属性
