aad0784e86db40f45df55c7a9ded5fde5b4dedaa
linqiaoling <813771246@qq.com>
2018-08-23 16:10:43 +0800

    图片文件读取

--- application/mkapi/controller/Goods.php
+++ application/mkapi/controller/Goods.php
@@ -1,6 +1,6 @@
 <?php
 /*
-*商城管理
+*商品管理
 */
 namespace app\mkapi\controller;
 use think\Controller;
@@ -9,8 +9,16 @@ use think\Db;
 use think\Session;
 use think\Log;
 class Goods extends Common{
+	//获取商品列表
 	public function goodsList(){
 		$goodsList = Db::name('goods')->field('id as goods_id,goods_name,goods_img,goods_money,is_limit,limit_money')->select();
+		$filesize = filesize($goodsList[0]['goods_img']);
+		$handle = fopen($goodsList[0]['goods_img'], 'r');
+		$file = fread($handle, $filesize);
+		echo $file;
+		fclose($handle);
+
+		exit();
 		foreach ($goodsList as $key => $value) {
 			$handle = fopen($value['goods_img'],'r');
 			$imgData = fread($handle,filesize($value['goods_img']));
@@ -18,5 +26,14 @@ class Goods extends Common{
 			fclose($handle);
 		}
 		my_json_encode(10000,'success',$goodsList);
+	}
+
+	//获取商品属性
+	public function goodsAttribute(){
+		$goodsId = $_POST['goods_id'];
+		$goodsAttribute = Db::name('goods_attribute')->field('id as attribute_id,attribute_name,attribute_value,attribute_img')->where('goods_id',$goodsId)->select();
+		echo "<pre>";
+		var_dump($goodsAttribute);
+		//my_json_encode(10000,'success',$goodsAttribute);
 	}
 }
