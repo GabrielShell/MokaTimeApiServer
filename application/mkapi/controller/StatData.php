b7ccef9419e0c2ab01dde5993858bc3fa20a14ad
Gabriel Shell <2794276764@qq.com>
2018-08-27 14:27:40 +0800

    7日内应还统计

--- application/mkapi/controller/StatData.php
+++ application/mkapi/controller/StatData.php
@@ -4,6 +4,7 @@ namespace app\mkapi\controller;
 use think\Controller;
 use think\Request;
 use app\mkapi\model\Users;
+use app\mkapi\model\Repay_plans;
 
 /**
  * 统计数据控制器
@@ -21,7 +22,13 @@ class StatData extends Common{
             return ['status'=>2,'msg'=>'series不能为空'];
         $userId = Users::where(['series'=>$userSeries])->value('id');
 
-        my_json_encode(0,'',['SevenDaysRepay'=>1000]);
+        $dayList = [date('Y-m-d')];
+        for($i = 1;$i <= 6;$i++){
+            $dayList[] = date('Y-m-d',strtotime('+' . $i.' days'));
+        }
+
+        $SevenSum = Repay_plans::where('user_id',$userId)->where('action','repay')->where('action_date','in',implode(',',$dayList))->sum('amount');
+        my_json_encode(0,'',['SevenDaysRepay'=>$SevenSum]);
     }
     
 }
