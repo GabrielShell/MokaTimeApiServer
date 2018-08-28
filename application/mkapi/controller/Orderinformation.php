88b3501cf5ee469df344e5c4f180425341f2117c
linqiaoling <813771246@qq.com>
2018-08-28 16:21:40 +0800

    修改订单消息

--- application/mkapi/controller/Orderinformation.php
+++ application/mkapi/controller/Orderinformation.php
@@ -9,7 +9,7 @@ class Orderinformation extends Common{
 	//订单消息
 	public function info(){
 		$series = $_POST['series'];
-		$orderList = Db::name('order_information')->field('a.id,a.content,a.order_id,c.goods_name,c.goods_img,a.update_time')->alias('a')->join('order b','a.order_id = b.id')->join('goods c','b.goods_id = c.id')->where('a.series',$series)->order('a.update_time desc')->select();
+		$orderList = Db::name('order_information')->field('a.id,a.content,a.order_id,b.delivery_no,c.goods_name,c.goods_img,a.update_time')->alias('a')->join('order b','a.order_id = b.id')->join('goods c','b.goods_id = c.id')->where('a.series',$series)->order('a.update_time desc')->select();
 		foreach ($orderList as $key => $value) {
 			$handle = fopen($value['goods_img'],'r');
 			$imgData = fread($handle,filesize($value['goods_img']));
