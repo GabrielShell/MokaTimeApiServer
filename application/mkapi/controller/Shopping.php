a73ef6fa57d4c49c31c9affc6c97f6acaad99ce8
linqiaoling <813771246@qq.com>
2018-09-05 13:18:50 +0800

    修改图片url

--- application/mkapi/controller/Shopping.php
+++ application/mkapi/controller/Shopping.php
@@ -146,7 +146,7 @@ class Shopping extends Common{
 			// 私有空间中的外链 http://<domain>/<file_key>
 			$name = substr($value1['goods_thumb'], 0,strpos($value1['goods_thumb'], '.'));
 	        $baseUrl = 'http://'.$domain.'/goods/'.$value1['goods_thumb'].'?imageMogr2/interlace/1/format/webp&attname='.$name.'.webp';
-			$orderList[$key1]['goods_thumb'] = $auth->privateDownloadUrl($baseUrl);
+			$orderList[$key1]['goods_thumb'] = $baseUrl;
 		}
 
 		my_json_encode(10000,'success',$orderList);
