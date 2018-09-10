6d11887e710f9bf0a7c8c6dd17acc96ac53e22c6
Gabriel Shell <2794276764@qq.com>
2018-09-10 14:54:23 +0800

    增加畅捷交易通知接口

---
+++ application/mkapi/controller/CjTradeNotice.php
@@ -0,0 +1,50 @@
+<?php
+namespace app\mkapi\controller;
+use think\Controller;
+use think\Request;
+use think\Db;
+use think\Session;
+use app\mkapi\model\Tradenotice as Tn;
+class CjTradeNotice extends Controller{
+    public function index(Request $request){
+        header('Content-type: application/json;charset=GBK');
+
+        $params = $request->param();
+        $res = json_encode($params);
+
+        $tn = new Tn;
+        $tn->content = $res;
+        $tn->save();
+
+        $returnArr = [
+            'tranTime' => array_key_exists('tranTime',$params) ? $params['tranTime'] : '',
+            'tranNo' => array_key_exists('tranNo',$params) ? $params['tranNo'] : '',
+            'rrn' => array_key_exists('rrn',$params) ? $params['rrn'] : '',
+            'merchantId' => array_key_exists('merchantId',$params) ? $params['merchantId'] : '',
+            'termId' => array_key_exists('termId',$params) ? $params['termId'] : '',
+            'responseCode' => '00',
+            'responseDesc' => '通知成功',
+            'extData' => '',
+        ];
+
+        $sign = self::sign($returnArr);
+        $returnArr['sign'] = $sign;
+        return json_encode($returnArr,JSON_UNESCAPED_UNICODE);
+    }
+
+    public static function sign($array){
+        ksort($array);
+        $valueStr = '';
+        foreach($array as $item){
+            $valueStr = $valueStr . $item;
+        }
+
+        //测试环境密钥串(key)
+        $key = '0123456789ABCDEFFEDCBA9876543210';
+
+        $valueStr = $key . $valueStr;
+        
+        return md5($valueStr);
+        
+    }
+}
---
+++ application/mkapi/model/Tradenotice.php
@@ -0,0 +1,9 @@
+<?php
+/**
+*mk_tradenotice表模型
+*/
+namespace app\mkapi\model;
+use think\Model;
+class Tradenotice extends Model{
+	
+}
